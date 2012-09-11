3899966b009998e4b49bdbbfb4bff2de2d97fd48
alangreene <afgreene@gmail.com>
2012-09-11 10:48:14 -0500

    Static1

---
+++ UC_version/_includes/footer.inc.php
@@ -0,0 +1,42 @@
+	
+
+	<div id="footer">
+		<div id="footer_inner">
+			
+			<ul>
+				<h6>Participating Institutions</h6>	
+				<li><a target="_blank" href="http://www.uchicago.edu/index.shtml">University of Chicago</a></li>
+				<li><a target="_blank" href="http://www.anl.gov/">Argonne National Laboratory</a></li>
+				<li><a target="_blank" href="http://www.uchicagoargonnellc.org/">UChicago/Argonne, LLC</a></li>
+			</ul>
+
+
+			<ul>
+				<h6>Argonne National Laboratory Divisions</h6>	
+				<li><a target="_blank" href="http://www.ne.anl.gov/">ANL Nuclear Engineering Division (NED)</a></li>
+				<li><a target="_blank" href="http://www.msd.anl.gov/">ANL Materials Science Division (MSD)</a></li>
+				<li><a target="_blank" href="http://cars.uchicago.edu/">Consortium for Advanced Radiation Sources (CARS)</a></li>
+				<li><a target="_blank" href="http://cars9.uchicago.edu/chemmat/index.html">ChemMat CARS</a></li>
+			</ul>
+
+
+			<ul>
+				<h6>Argonne National Laboratory Facilities</h6>	
+				<li><a target="_blank" href="http://www.aps.anl.gov/">Advanced Photon Source (APS)</a></li>
+				<li><a target="_blank" href="http://www.msd.anl.gov/groups/emc/">Electron Microscopy Center (EMC)</a></li>
+			</ul>
+
+
+			<ul>
+				<h6>Archaeological Projects</h6>	
+				<li><a target="_blank" href="http://www.kaogu.cn/en/index.asp">Chinese National Institute of Archaeology (IA, CASS)</a></li>
+				<li><a target="_blank" href="http://www.kaogu.cn/en/detail.asp?ProductID=1132">Yi Luo River Valley Survey Project (YRV)</a></li>
+				<li><a target="_blank" href="http://www.saie.wustl.edu/">Central Asian Archaeology and Spatial Analysis Laboratory (SAIE)</a></li>
+				<li id="last">Copyright 2012, MAE Project</li>
+				<li id="phlabs"><a target="_blank" href="http://www.phlabs.net/" class="phlabs icon">website by Ph Labs</a></li>
+
+			</ul>
+
+
+		</div><!--end footer_inner-->
+	</div><!--end footer-->
