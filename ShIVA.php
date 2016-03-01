6564d6752b4be9724140f44c6333050151a8a3c2
alangreene <afgreene@gmail.com>
2016-02-29 23:18:31 -0500

    edits and C++ enhancement to ShIVA page

--- ShIVA.php
+++ ShIVA.php
@@ -15,14 +15,15 @@
 			<div id="content_inner">
 			
 				<h2>Sherd Image Viewer and Analysis (ShIVA)</h2>
-                <p> ShIVA is written in Interactive Data Langauge (IDL) and is available for download as a freestanding application for use in Apple OSX, Linux, and Microsoft Windows. The software relies on the free <a href="http://www.exelisvis.com/Support/HelpArticlesDetail/TabId/219/ArtMID/900/ArticleID/12395/The-IDL-Virtual-Machine.aspx">IDL Virtual Machine</a>, included in the download package.</p>
+                <p> ShIVA is written in Interactive Data Langauge (IDL) and is available for download as a freestanding application for use in Apple OSX, Linux, and Microsoft Windows. The software relies on the free <a href="http://www.exelisvis.com/Support/HelpArticlesDetail/TabId/219/ArtMID/900/ArticleID/12395/The-IDL-Virtual-Machine.aspx">IDL Virtual Machine</a>, also included in the download package.</p>
                 <p>All questions regarding proper use of ShIVA, bugs, and future development should be directed to <a href="mailto:maetomorad@gmail.com">maetomorad at gmail dot com</a>. Correspondence is encouraged, as the software remains in a stage of active development and code enhancement. Don't be shy!</p>
                 <h3>Download Links:</h3>
                 <p><a href="ShIVA_windows.zip">Windows</a></p>
+                <p>NOTE: For proper use of ShIVA in Windows, your computer must have an updated version of the <a href="https://www.microsoft.com/en-us/download/details.aspx?id=26999">C++ redistributable package update for Visual Studio 2010</a> installed. Visit the previous link and select "Download," followed by the version of the package that matches your system configuration. After the download is complete, run the resultant installer.
                 <p><a href="ShIVA.dmg">Mac/OSX</a></p>
                 <p>NOTE: For proper use of ShIVA in OSX, your computer must be <a href="http://www.macworld.co.uk/how-to/mac-software/how-show-hidden-files-in-mac-os-x-finder-funter-3520878/">set to show hidden files</a>.
                 <p><a href="ShIVA.pro">Linux</a></p>
-                <p>The software can also be downloaded from <a href="sourceforge.net">sourceforge.net</a>.</p>
+                <p>The software can also be downloaded from <a href="http://sourceforge.net">sourceforge.net</a>.</p>
                 
 
 
