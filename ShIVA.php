7cedae2340ce4700179e564880ff24407dba8d9c
alangreene <afgreene@gmail.com>
2016-03-09 13:09:16 -0800

    add XQuartz and add new tab links to ShIVA page

--- ShIVA.php
+++ ShIVA.php
@@ -15,16 +15,16 @@
 			<div id="content_inner">
 			
 				<h2>Sherd Image Viewer and Analysis (ShIVA)</h2>
-                <p> ShIVA is a software tool for viewing, manipulating, and extracting qualitative and quantitative data from digital radiographs and tomograms of archaeological materials. The program is written in Interactive Data Langauge (IDL) and is available for download as a freestanding application for use in Apple OSX, Linux, and Microsoft Windows. It relies on the free <a href="http://www.exelisvis.com/Support/HelpArticlesDetail/TabId/219/ArtMID/900/ArticleID/12395/The-IDL-Virtual-Machine.aspx">IDL Virtual Machine</a>, also included in the download package.</p>
+                <p> ShIVA is a software tool for viewing, manipulating, and extracting qualitative and quantitative data from digital radiographs and tomograms of archaeological materials. The program is written in Interactive Data Langauge (IDL) and is available for download as a freestanding application for use in Apple OSX, Linux, and Microsoft Windows. It relies on the free <a href="http://www.exelisvis.com/Support/HelpArticlesDetail/TabId/219/ArtMID/900/ArticleID/12395/The-IDL-Virtual-Machine.aspx" target="_blank">IDL Virtual Machine</a>, also included in the download package.</p>
                 <p>The ShIVA verions available here are configured to output tabular data as text files. If you are interested in using ShIVA with a specific database platform or in concert with MAE's ReArch data management software, please contact us.</p>
             <p>All questions regarding proper use of ShIVA, bugs, and future development should be directed to <a href="mailto:maetomorad@gmail.com">maetomorad at gmail dot com</a>. Correspondence is encouraged, as the software remains in a stage of active development and code enhancement. Don't be shy!</p>
                 <h2>Download Links:</h2>
                 <p><a href="ShIVA_windows.zip">Windows</a><br />
-                NOTE: For proper use of ShIVA in Windows, your computer must have an updated version of the <a href="https://www.microsoft.com/en-us/download/details.aspx?id=26999">C++ redistributable package update for Visual Studio 2010</a> installed. Visit the previous link and select "Download," followed by the version of the package that matches your system configuration. After the download is complete, run the resultant installer.</p>
+                NOTE: For proper use of ShIVA in Windows, your computer must have an updated version of the <a href="https://www.microsoft.com/en-us/download/details.aspx?id=26999" target="_blank">C++ redistributable package update for Visual Studio 2010</a> installed. Visit the previous link and select "Download," followed by the version of the package that matches your system configuration. After the download is complete, run the resultant installer.</p>
                 <p><a href="ShIVA.dmg">Mac/OSX</a><br />
-                NOTE: For proper use of ShIVA in OSX, your computer must be <a href="http://www.macworld.co.uk/how-to/mac-software/how-show-hidden-files-in-mac-os-x-finder-funter-3520878/">set to show hidden files</a>.</p>
+                NOTE: For proper use of ShIVA in OSX, your computer must have <a href="http://www.xquartz.org" target="_blank">XQuartz</a> installed and be <a href="http://www.macworld.co.uk/how-to/mac-software/how-show-hidden-files-in-mac-os-x-finder-funter-3520878/" target="_blank">set to show hidden files</a>.</p>
                <p> <a href="ShIVA.pro">Linux</a><br />
-                The software can also be downloaded from <a href="http://sourceforge.net">sourceforge.net</a>.</p>
+                The software can also be downloaded from <a href="http://sourceforge.net" target="_blank">sourceforge.net</a>.</p>
                 
 
 
